cc2d84ec77228dda2fb67bf457aa70cbe4169a4c
Onur Kerimov <onurkerimov@gmail.com>
2024-10-10 19:46:26 +0300

    Improve library config

--- packages/my--other-library/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from './components/Button'
-export { Input } from './components/Input'
-export { Label } from './components/Label'
--- vite.library.config.ts
+++ vite.library.config.ts
@@ -1,49 +1,130 @@
-import { extname, relative, resolve } from 'path'
+import { extname, relative, resolve, join } from 'path'
 import { fileURLToPath } from 'node:url'
 import { glob } from 'glob'
 import dts from 'vite-plugin-dts'
-// import { libInjectCss } from 'vite-plugin-lib-inject-css'
-
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import type { UserConfig } from 'vite'
+import { getPackagesSync } from '@manypkg/get-packages'
+import { viteStaticCopy, Target } from 'vite-plugin-static-copy'
+import fs from 'fs'
+import fse from 'fs-extra'
+
+const ROOT_DIR = process.cwd()
+const PACKAGES_DIR = 'packages'
+const TEMP_DIR = 'temp'
+
+const { packages } = getPackagesSync(ROOT_DIR)
 
 const createLibConfig = (): UserConfig => {
-  const packageName = 'my-library'
-  const packageSrc = `packages/${packageName}/src`
+  const libInputs = {}
+  const copyTargets: Target[] = []
 
-  const libInputs = Object.fromEntries(
-    // https://rollupjs.org/configuration-options/#input
-    glob.sync(packageSrc + '/**/*.{ts,tsx}').map((file) => [
-      // 1. The name of the entry point
-      // lib/nested/foo.js becomes nested/foo
-      relative(packageSrc, file.slice(0, file.length - extname(file).length)),
-      // 2. The absolute path to the entry file
-      // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-      fileURLToPath(new URL(file, import.meta.url)),
-    ]),
-  )
+  // Function to add file to copy targets if it exists
+  const copyFileIfExists = (filePath: string, relativeDir: string) => {
+    if (glob.sync(filePath).length > 0) {
+      copyTargets.push({
+        src: filePath,
+        dest: relative(PACKAGES_DIR, relativeDir),
+      })
+    }
+  }
+
+  packages.forEach((pkg) => {
+    const packageSrc = `${pkg.relativeDir}/src`
+    glob.sync(packageSrc + '/**/*.{ts,tsx}').map((file) => {
+      const key = relative(
+        PACKAGES_DIR,
+        file.slice(0, file.length - extname(file).length),
+      )
+      const value = fileURLToPath(new URL(file, import.meta.url))
+      libInputs['temp/' + key] = value
+    })
+
+    // Add package.json and readme.md to copy targets if they exist
+    copyFileIfExists(`${pkg.relativeDir}/package.json`, pkg.relativeDir)
+    copyFileIfExists(`${pkg.relativeDir}/readme.md`, pkg.relativeDir)
+  })
 
   return {
     plugins: [
-      //libInjectCss(),
-      dts({ include: [packageSrc] }),
+      libInjectCss(),
+      dts({
+        include: ['packages'],
+        outDir: 'dist/temp',
+      }),
+      viteStaticCopy({
+        targets: copyTargets,
+      }),
+      {
+        name: 'post-build-copy',
+        closeBundle: () => {
+          copyFiles()
+        },
+      },
     ],
     build: {
-      outDir: `dist/${packageName}`,
+      outDir: `dist`,
       copyPublicDir: false,
       lib: {
-        entry: resolve(__dirname, packageSrc),
+        entry: resolve(ROOT_DIR, PACKAGES_DIR),
         formats: ['es'],
       },
       rollupOptions: {
-        external: ['react', 'react/jsx-runtime'],
+        external: (id, parentId, isResolved) => {
+          if (!parentId) return false
+          if (isResolved) return false
+          if (id.charAt(0) === '.') return false
+          return true
+        },
         input: libInputs,
         output: {
-          assetFileNames: `assets/[name][extname]`,
+          assetFileNames: (info) => {
+            let assetDir = info.originalFileNames?.[0]
+            if (assetDir)
+              assetDir =
+                relative(PACKAGES_DIR, assetDir).replace(/\/[^/]*$/, '') + '/'
+            return `temp/${assetDir}[name][extname]`
+          },
           entryFileNames: `[name].js`,
         },
       },
     },
   }
+}
+
+const copyFiles = () => {
+  const sourceDir = join(ROOT_DIR, 'dist', TEMP_DIR)
+
+  // Function to copy files
+  function traverse(src: string) {
+    fs.readdirSync(src, { withFileTypes: true }).forEach((dirent) => {
+      const srcPath = join(src, dirent.name)
+
+      if (dirent.isDirectory()) {
+        if (dirent.name === 'src') {
+          // Copy all files in the 'src' folder
+          fs.readdirSync(srcPath).forEach((file) => {
+            const filePath = join(srcPath, file)
+            const packageName = relative(
+              ROOT_DIR + '/dist/' + TEMP_DIR,
+              srcPath,
+            ).split('/')[0]
+            const destinationFilePath = join(
+              ROOT_DIR,
+              'dist',
+              packageName,
+              file,
+            )
+            fse.copy(filePath, destinationFilePath, { overwrite: true })
+          })
+        } else {
+          // Recursively handle subdirectories
+          traverse(srcPath)
+        }
+      }
+    })
+  }
+  traverse(sourceDir)
 }
 
 export default createLibConfig()
